9ec1ea7947f0416baf7f8a30a7b8769c1ea296f1
dfbarletta <89224650+dfbarletta@users.noreply.github.com>
2021-11-28 16:38:46 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -4,13 +4,13 @@ var contador = 1;
 function main() {
     $('.menu-bar').click(function(){
         if(contador==1){
-            $('.menu').animate({
+            $('nav.menu').animate({
               left= '0' 
         });
             contador=0;
         }else{
             contador=1;
-           $('.menu').animate({
+           $('nav.menu').animate({
               left= '-100%'            
            });
         }
