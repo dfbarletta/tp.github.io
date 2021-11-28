f5d93a71d9eb72a8963b50a34df4e13728150701
dfbarletta <89224650+dfbarletta@users.noreply.github.com>
2021-11-28 16:15:12 -0300

    Update js.js

--- js.js
+++ js.js
@@ -14,21 +14,4 @@
     }, false);
 })();
 
-$(document).ready(main);
-var contador = 1;
 
-function main() {
-    $('.menu-bar').click(function(){
-        if(contador==1){
-            $('.menu').animate({
-              left= '0' 
-    });
-            contador=0;
-        }else{
-            contador=1;
-           $('.menu').animate({
-              left= '-100%'            
-    });
-    }
-    });
-};
