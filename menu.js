1a23ffc9e209699a0f48d3bab58a01715113f91e
dfbarletta <89224650+dfbarletta@users.noreply.github.com>
2021-11-28 16:30:03 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,18 @@
+$(document).ready(main);
+var contador = 1;
+
+function main() {
+    $('.menu-bar').click(function(){
+        if(contador==1){
+            $('.menu').animate({
+              left= '0' 
+    });
+            contador=0;
+        }else{
+            contador=1;
+           $('.menu').animate({
+              left= '-100%'            
+    });
+    }
+    });
+};
