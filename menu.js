fe9cd731bd3b381c1a6ff9aa3e6de43f4f3cca47
dfbarletta <89224650+dfbarletta@users.noreply.github.com>
2021-11-28 22:54:37 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,5 +1,5 @@
-const menu-bar = document.querySelector('.menu-bar');
-const menu = document.querySelector('.menu');
+const menu-bar = document.querySelector('#menu-bar');
+const menu = document.querySelector('#menu');
 menu-bar.addEventListener('click', function(){
 menu.classList.toggle('mostrar');
 });
